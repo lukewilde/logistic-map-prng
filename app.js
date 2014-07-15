3d4e3066865b495f824e641a14748275db4e5e69
Luke Wilde <luke.wilde@clock.co.uk>
2014-07-15 19:05:02 +0100

    Adjusting specifity of module.

--- app.js
+++ app.js
@@ -1,56 +1,27 @@
-// var levelSeed = 51748
-var levelSeeds = [51748, 76294, 52836, 97264, 81537]
-  , dimensions =
-    {  width: 600
-    ,  height: 400
+module.exports = function createRandomSequence(seed) {
+
+  var generator =
+      { initialSeed: seed ? '0.' + seed : Math.random()
+      }
+
+  generator.get = function() {
+    return generator.currentSeed = logisticMap(generator.currentSeed)
+  }
+
+  function logisticMap(x) {
+    return 4 * x * (1 - x)
+  }
+
+  function init() {
+
+    generator.currentSeed = generator.initialSeed
+
+    for (var i = 0; i < 100; i++) {
+      generator.currentSeed = logisticMap(generator.currentSeed)
     }
 
-function generateLevel(levelSeed) {
-
-  var totalPoints = Math.floor(levelSeed / 4000)
-    , points = []
-    , chaosPoints = getChaos(levelSeed, totalPoints * 2)
-
-  // console.log('Generating ' + totalPoints + ' coordinates')
-  // console.log('Generating chaos: ', chaosPoints)
-
-  for (var i = 0; i < totalPoints; i++) {
-
-      var point =
-        { x: Math.floor(chaosPoints[i] * dimensions.width)
-        , y: Math.floor(chaosPoints[i + 10] * dimensions.height)
-        }
-
-    points[i] = point
+    return generator
   }
 
-  return points
+  return init()
 }
-
-function logisticMap(x) {
-  return 4 * x * (1 - x)
-}
-
-function getChaos(seed, total) {
-
-  var data = []
-   , currentSeed = '0.' + seed
-
-   // 100 iterations will provide adequate randomness
-  for (var i = 0; i < 100 + total; i++) {
-    currentSeed = logisticMap(currentSeed)
-    data.push(currentSeed)
-  }
-
-  return data.splice(100, total)
-}
-
-var levels = []
-
-levelSeeds.forEach(function(seed) {
-  levels.push(generateLevel(seed))
-})
-
-console.log('payload', levels)
-
-// console.log('payload', generateLevel(levelSeed))
